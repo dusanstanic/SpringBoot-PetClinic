a7239f2cd626e50093689c0c10b1ecae5f70b85a
Dusan <dusan.stanic97@hotmail.com>
2020-05-15 18:02:09 +0200

    Refactored OwnerServiceMap method

--- petClinicData/src/main/java/com/springframwork/petClinic/services/map/OwnerServiceMap.java
+++ petClinicData/src/main/java/com/springframwork/petClinic/services/map/OwnerServiceMap.java
@@ -36,8 +36,6 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 	@Override
 	public Owner save(Owner object) {
 
-		Owner saveOwner = null;
-
 		if (object != null) {
 			if (object.getPets() != null) {
 				object.getPets().forEach(pet -> {
